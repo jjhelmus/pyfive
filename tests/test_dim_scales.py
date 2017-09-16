fe71d7b919a8d13af51e5cca3c92e8569d06313c
Jonathan Helmus <jjhelmus@gmail.com>
2017-09-16 12:12:30 -0500

    TST: unit tests for dim scales and labels

---
+++ tests/test_dim_scales.py
@@ -0,0 +1,64 @@
+""" Unit tests for pyfive dimension scales. """
+
+import os
+
+from numpy.testing import assert_array_equal
+
+import pyfive
+
+DIRNAME = os.path.dirname(__file__)
+DIM_SCALES_HDF5_FILE = os.path.join(DIRNAME, './dim_scales.hdf5')
+
+
+def test_dim_labels():
+
+    hfile = pyfive.File(DIM_SCALES_HDF5_FILE)
+
+    # dataset with dimension labels
+    dims = hfile['dset1'].dims
+    assert dims[0].label == 'z'
+    assert dims[1].label == 'y'
+    assert dims[2].label == 'x'
+
+    # dataset with no dimension labels
+    dims = hfile['dset2'].dims
+    assert dims[0].label == ''
+    assert dims[1].label == ''
+    assert dims[2].label == ''
+
+    hfile.close()
+
+
+def test_dim_scales():
+
+    hfile = pyfive.File(DIM_SCALES_HDF5_FILE)
+
+    # dataset with dimension scales
+    dims = hfile['dset1'].dims
+
+    assert len(dims) == 3
+
+    assert len(dims[0]) == 1
+    assert len(dims[1]) == 1
+    assert len(dims[2]) == 2
+
+    assert dims[0][0].name == '/z1'
+    assert dims[1][0].name == '/y1'
+    assert dims[2][0].name == '/x1'
+    assert dims[2][1].name == '/x2'
+
+    assert_array_equal(dims[0][0][:], [0, 10, 20, 30])
+    assert_array_equal(dims[1][0][:], [3, 4, 5])
+    assert_array_equal(dims[2][0][:], [1, 2])
+    assert_array_equal(dims[2][1][:], [99, 98])
+
+    # dataset with no dimension scales
+    dims = hfile['dset2'].dims
+
+    assert len(dims) == 3
+
+    assert len(dims[0]) == 0
+    assert len(dims[1]) == 0
+    assert len(dims[2]) == 0
+
+    hfile.close()
