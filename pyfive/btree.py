1e9f3e5da0a3a0515b3c949cd6c50ba183f2d5d2
woutdenolf <woutdenolf@users.sf.net>
2021-07-12 20:43:25 +0200

    Add partial support for B-tree v2

--- pyfive/btree.py
+++ pyfive/btree.py
@@ -257,6 +257,190 @@ class BTreeV1RawDataChunks(BTreeV1):
         return True
 
 
+class BTreeV2(AbstractBTree):
+    """
+    HDF5 version 2 B-Tree.
+    """
+
+    # III.A.2. Disk Format: Level 1A2 - Version 2 B-trees
+    B_TREE_HEADER = OrderedDict((
+        ('signature', '4s'),
+
+        ('version', 'B'),
+        ('node_type', 'B'),
+        ('node_size', 'I'),
+        ('record_size', 'H'),
+        ('depth', 'H'),
+        ('split_percent', 'B'),
+        ('merge_percent', 'B'),
+
+        ('root_address', 'Q'),     # 8 byte addressing
+        ('root_nrecords', 'H'),
+        ('total_nrecords', 'Q'),     # 8 byte addressing
+    ))
+
+    B_LINK_NODE = OrderedDict((
+        ('signature', '4s'),
+
+        ('version', 'B'),
+        ('node_type', 'B'),
+    ))
+
+    def _read_root_node(self):
+        h = self._read_tree_header(self.offset)
+        self.address_formats = self._calculate_address_formats(h)
+        self.header = h
+        self.depth = h["depth"]
+
+        address = (h["root_address"], h["root_nrecords"], h["total_nrecords"])
+        root_node = self._read_node(address, self.depth)
+        self._add_node(root_node)
+
+    def _read_tree_header(self, offset):
+        self.fh.seek(self.offset)
+        header = _unpack_struct_from_file(self.B_TREE_HEADER, self.fh)
+        assert header['signature'] == b'BTHD'
+        assert header['node_type'] == self.NODE_TYPE
+        return header
+
+    def _calculate_address_formats(self, header):
+        node_size = header["node_size"]
+        record_size = header["record_size"]
+
+        nrecords_max = 0
+        ntotalrecords_max = 0
+        address_formats = []
+        for node_level in range(header["depth"]+1):
+            offset_fmt = ""
+            num1_fmt = ""
+            num2_fmt = ""
+            if node_level == 0:  # leaf node
+                offset_size = 0
+                num1_size = 0
+                num2_size = 0
+            elif node_level == 1:  # internal node (twig node)
+                offset_size = 8
+                offset_fmt = "<Q"
+                num1_size = self._required_bytes(nrecords_max)
+                num1_fmt = "<{}s".format(num1_size)
+                num2_size = 0
+            else:  # internal node
+                offset_size = 8
+                offset_fmt = "<Q"
+                num1_size = self._required_bytes(nrecords_max)
+                num1_fmt = "<{}s".format(num1_size)
+                num2_size = self._required_bytes(ntotalrecords_max)
+                num2_fmt = "<{}s".format(num2_size)
+            address_formats.append((
+                offset_size, num1_size, num2_size,
+                offset_fmt, num1_fmt, num2_fmt))
+            if node_level < header["depth"]:
+                addr_size = offset_size + num1_size + num2_size
+                nrecords_max = self._nrecords_max(node_size, record_size, addr_size)
+                if ntotalrecords_max:
+                    ntotalrecords_max *= nrecords_max
+                else:
+                    ntotalrecords_max = nrecords_max
+
+        return address_formats
+
+    @staticmethod
+    def _nrecords_max(node_size, record_size, addr_size):
+        """ Calculate the maximal records a node can contain. """
+        # node_size = overhead + nrecords_max*record_size + (nrecords_max+1)*addr_size
+        #
+        # overhead = size(B_LINK_NODE) + 4 (checksum)
+        #
+        # Leaf node (node_level = 0)
+        #   addr_size = 0
+        # Internal node (node_level = 1)
+        #   addr_size = offset_size + num1_size
+        # Internal node (node_level > 1)
+        #   addr_size = offset_size + num1_size + num2_size
+        return (node_size - 10 - addr_size)//(record_size + addr_size)
+
+    @staticmethod
+    def _required_bytes(integer):
+        """ Calculate the minimal required bytes to contain an integer. """
+        return (max(integer.bit_length(), 1) + 7) // 8
+
+    def _read_node(self, address, node_level):
+        """ Return a single node in the B-Tree located at a given offset. """
+        offset, nrecords, ntotalrecords = address
+        node = self._read_node_header(offset, node_level)
+
+        record_size = self.header['record_size']
+
+        keys = []
+        for _ in range(nrecords):
+            record = self.fh.read(record_size)
+            keys.append(self._parse_record(record))
+
+        addresses = []
+        fmts = self.address_formats[node_level]
+        if fmts[0]:
+            offset_size, num1_size, num2_size, offset_fmt, num1_fmt, num2_fmt = fmts
+            for _ in range(nrecords+1):
+                offset = struct.unpack(offset_fmt, self.fh.read(offset_size))[0]
+                num1 = struct.unpack(num1_fmt, self.fh.read(num1_size))[0]
+                num1 = int.from_bytes(num1, byteorder="little", signed=False)
+                if num2_size:
+                    num2 = struct.unpack(num2_fmt, self.fh.read(num2_size))[0]
+                    num2 = int.from_bytes(num2, byteorder="little", signed=False)
+                else:
+                    num2 = num1
+                addresses.append((offset, num1, num2))
+
+        node['keys'] = keys
+        node['addresses'] = addresses
+        return node
+
+    def _read_node_header(self, offset, node_level):
+        """ Return a single node header in the b-tree located at a give offset. """
+        self.fh.seek(offset)
+        node = _unpack_struct_from_file(self.B_LINK_NODE, self.fh)
+        assert node['node_type'] == self.NODE_TYPE
+        if node_level:
+            # Internal node (has children)
+            assert node['signature'] == b'BTIN'
+        else:
+            # Leaf node (has no children)
+            assert node['signature'] == b'BTLF'
+        node["node_level"] = node_level
+        return node
+
+    def iter_records(self):
+        """ Iterate over all records. """
+        for nodelist in self.all_nodes.values():
+            for node in nodelist:
+                yield from node["keys"]
+
+    def _parse_record(self, record):
+        raise NotImplementedError
+
+
+class BTreeV2GroupNames(BTreeV2):
+    """
+    HDF5 version 2 B-Tree storing group names (type 5).
+    """
+    NODE_TYPE = 5
+
+    def _parse_record(self, record):
+        namehash = struct.unpack_from("<I", record, 0)[0]
+        return {'namehash': namehash, 'heapid':record[4:4+7]}
+
+
+class BTreeV2GroupOrders(BTreeV2):
+    """
+    HDF5 version 2 B-Tree storing group creation orders (type 6).
+    """
+    NODE_TYPE = 6
+
+    def _parse_record(self, record):
+        creationorder = struct.unpack_from("<Q", record, 0)[0]
+        return {'creationorder': creationorder, 'heapid':record[8:8+7]}
+
+
 # IV.A.2.l The Data Storage - Filter Pipeline message
 RESERVED_FILTER = 0
 GZIP_DEFLATE_FILTER = 1
