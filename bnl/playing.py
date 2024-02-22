02fca542f531901b565116ec893a1b0d1d59cb20
Bryan Lawrence <bryan.lawrence@ncas.ac.uk>
2024-02-22 12:20:54 +0000

    Using s3 to get at some real data for testing

--- bnl/playing.py
+++ bnl/playing.py
@@ -1,18 +1,35 @@
 import pyfive
 from pathlib import Path
 from pyfive.as_dataobjects import ADataObjects
+import s3fs 
+import h5py
 
 MYFILE = 'da193o_25_day__grid_T_198807-198807.nc'
-MYFILE = '../tests/chunked.hdf5'
+#MYFILE = '../tests/chunked.hdf5'
+MYFILE =  'da193a_25_day__198807-198807.nc'
+
 MYPATH = Path(__file__).parent
 
 #f = h5py.File(MYPATH/MYFILE,'r')
-f2 = pyfive.File(MYPATH/MYFILE)
-path = 'dataset1'
-link_target = f2._links[path]
-dsref = ADataObjects(f2.file._fh, link_target)
-chunk_index = dsref.get_offset_addresses()
-print(chunk_index)
+
+S3_URL = 'https://uor-aces-o.s3-ext.jc.rl.ac.uk/'
+fs = s3fs.S3FileSystem(anon=True, client_kwargs={'endpoint_url': S3_URL})
+uri = 'bnl/'+MYFILE
+
+with fs.open(uri,'rb') as s3file2:
+    f2 = pyfive.File(s3file2)
+    #f2 = pyfive.File(MYPATH/MYFILE)
+    path = 'dataset1'
+    path = 'tos'
+    path = 'm01s06i247_4'
+    link_target = f2._links[path]
+    dsref = ADataObjects(f2.file._fh, link_target)
+    chunk_index = dsref.get_offset_addresses()
+    for e in chunk_index:
+        print(e)
+with fs.open(uri,'rb') as s3file2:
+    f3 = h5py.File(s3file2,'r')
+    print(f3[path])
 
 
 #v='tos'
